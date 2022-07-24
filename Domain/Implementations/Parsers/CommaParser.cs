6fab1d37f6fb8a53ef25ccebb48b8d169a913cb2
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-25 10:53:54 +1200

    Implementation of comma parser

---
+++ Domain/Implementations/Parsers/CommaParser.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Models;
+using Domain.Services;
+
+namespace Domain.Implementations.Parsers
+{
+    public class CommaParser<T> : IParser<T>
+    {
+        private readonly ILogger logger;
+        private readonly IMapper<T> mapper;
+
+        public CommaParser(ILogger logger, IMapper<T> mapper)
+        {
+            this.logger = logger;
+            this.mapper = mapper;
+        }
+
+         private static Traderecord Transformer(TradeRecord trade)
+            => new Traderecord{
+                Id = trade.Id.ToString(),
+                ClientName = trade.ClientName.ToString(),
+                ItemName = trade.ItemName.ToString(),
+                ItemQuantity = int.Parse(trade.ItemQuantity.ToString()),
+                UnitPrice = trade.UnitPrice.ToString(),
+                TotalPrice = trade.TotalPrice.ToString()
+            };
+
+        public IEnumerable<T> Parse(IEnumerable<string> records)
+        {
+             int lineNumber = 1; 
+            ICollection<TradeRecord> validatedTradeRecords = new List<TradeRecord>();
+            foreach (string unvalidatedTradeRecord in records)
+            {
+                //splitting along the comma (we assume that the fields are separated using commas)
+                var recordFields = unvalidatedTradeRecord.Split(",").ToList();
+                
+                if(recordFields.Count != 6){
+                    logger.Log($"Malformed record line on line number {lineNumber}"); 
+                     continue;
+                }
+                if(recordFields[0].Length != 6){
+                    logger.Log($"Malformed Id on line number {lineNumber}");
+                    continue;
+                }
+                if(recordFields[1].Length > 20){
+                    logger.Log($"Malformed ClientName on line number {lineNumber}");
+                    continue;
+                }
+                if(recordFields[2].Length > 10){
+                    logger.Log($"Malformed ItemName on line number {lineNumber}");
+                    continue;
+                }
+                if(!int.TryParse(recordFields[3], out int _)){
+                    logger.Log($"Malformed Quantity value on line number {lineNumber}");
+                    continue;
+                }
+                if(recordFields[4].Length <= 3){
+                    logger.Log($"Malformed UnitPrice currency on line number {lineNumber}");
+                    continue;
+                }
+                if(recordFields[5].Length <= 3) {
+                    logger.Log($"Malformed Total Price currency on line number {lineNumber}");
+                    continue;
+                }
+
+            //    validatedTradeRecords.Add(mapper.Map(recordFields));
+                lineNumber++;
+            }
+
+         //   return validatedTradeRecords.Select(Transformer);
+         return null;
+        }
+    }
+}
